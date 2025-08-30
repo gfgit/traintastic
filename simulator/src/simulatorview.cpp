c6c6b8b4150870a7324329883ea7fc56776a39ce
Filippo Gentile <filippogentile@disroot.org>
2025-08-30 20:43:37 +0200

    Simulator: added TrainsModel

--- simulator/src/simulatorview.cpp
+++ simulator/src/simulatorview.cpp
@@ -1175,6 +1175,9 @@ void SimulatorView::contextMenuEvent(QContextMenuEvent *e)
 
   QMenu *m = new QMenu(this);
   QAction *copySegData = m->addAction(tr("Copy segment data"));
+  copySegData->setEnabled(!m_stateData.powerOn);
+  copySegData->setVisible(!m_stateData.powerOn);
+
   QAction *result = m->exec(e->globalPos());
   if(result == copySegData)
   {
