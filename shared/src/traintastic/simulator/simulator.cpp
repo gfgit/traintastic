b2ae27f87662c67a089fea52fdfbf1cde1385f2f
Filippo Gentile <filippogentile@disroot.org>
2025-09-04 09:01:23 +0200

    Simulator: set max speed on signal aspect change

--- shared/src/traintastic/simulator/simulator.cpp
+++ shared/src/traintastic/simulator/simulator.cpp
@@ -816,6 +816,8 @@ void Simulator::receive(const SimulatorProtocol::Message& message, size_t fromCo
             s->lights[i].state = MainSignal::Light::State::Off;
           }
         }
+
+        s->maxSpeed = m.speed;
         break;
       }
       break;
